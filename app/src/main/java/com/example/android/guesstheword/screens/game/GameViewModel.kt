0e5ab5ac162069dc7a5c6440b5fd0de837a66bc2
aditi <aditid11@gmail.com>
2020-06-17 17:33:22 +0530

    Removed observers for timer , used binding for timer in layout file.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel()
@@ -47,6 +49,10 @@ class GameViewModel : ViewModel()
     val score:LiveData<Int> get() = _score
 
     val currentTime:LiveData<Long> get() = _currentTime
+
+    val currentTimeString  = Transformations.map(currentTime,{
+        time -> DateUtils.formatElapsedTime(time)
+    })
 
     // The list of words - the front of the list is the next word to guess
     lateinit var wordList: MutableList<String>
